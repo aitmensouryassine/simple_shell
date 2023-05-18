d7807585680af099dc1269faee9f1afed03e3c46
Yassine AIT MENSOUR <aitmensouryassine@outlook.com>
2023-05-18 20:06:32 +0000

    Created a seperate file for execve function

---
+++ _execve.c
@@ -0,0 +1,26 @@
+#include <unisted.h>
+#include <sys/wait.h>
+
+/**
+ * _execve - execute program with a child process
+ * @cmd: command name
+ * @args: command args
+ * @env: environemnt variable
+ * @name: program name
+ */
+void _execve(char *cmd, char **args, char **env, char *name)
+{
+	pid_t pid = fork();
+
+	if (pid == -1)
+		perror("Error forking parent process");
+	else if (pid == 0)
+	{
+		if (execve(cmd, args, env))
+			perror(name);
+	}
+	else
+	{
+		wait(NULL);
+	}
+}
