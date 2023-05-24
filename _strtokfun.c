38a7fe64a02b52bf4816b485973db14a4d810638
Hajarhoulal <hajarhl00@outlook.com>
2023-05-24 05:10:08 -0700

    To check if a character matchs any other character

--- _strtokfun.c
+++ _strtokfun.c
@@ -1,1 +1,66 @@
-aaa
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _check_char - checks if a character matchs any othet character
+ * @s: character to check
+ * @str: string of delimiters
+ * Return: 1 on success, 0 on failure
+ */
+unsigned int _check_char(char s, const char *str)
+{
+		unsigned int i;
+
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			if (s == str[i])
+				return (1);
+		}
+		return (0);
+}
+
+/**
+ * _strtok - extracts tokens of a string
+ * @str: string
+ * @delim: delimiter
+ * Return: A pointer to the next token or NULL
+ */
+char *_strtok(char *str, const char *delim)
+{
+	static char *tokens;
+	static char *new_token;
+	unsigned int i;
+
+		if (str != NULL)
+			new_token = str;
+			tokens = new_token;
+		if (tokens == NULL)
+			return (NULL);
+		for (i = 0; tokens[i] != '\0'; i++)
+		{
+			if (_check_char(tokens[i], delim) == 0)
+				break;
+		}
+		if (new_token[i] == '\0' || new_token[i] == '#')
+		{
+			new_token = NULL;
+			return (NULL);
+		}
+		tokens = new_token + i;
+		new_token = tokens;
+		for (i = 0; new_token[i] != '\0'; i++)
+		{
+			if (_check_char(new_token[i], delim) == 1)
+				break;
+		}
+		if (new_token[i] == '\0')
+			new_token = NULL;
+		else
+		{
+			new_token[i] = '\0';
+			new_token = new_token + i + 1;
+			if (*new_token == '\0')
+				new_token = NULL;
+		}
+		return (tokens);
+}
